267e7e586b83867bed157a8c44010a821198a29c
benedicta912 <benedictaokonkwo3594@gmail.com>
2021-06-21 15:05:44 +0100

    initial commit

---
+++ app/src/main/java/com/chinye/todoapp/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.chinye.todoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+
+
+    val userName : String = "Admin"
+    val passWord : String = "Admin"
+
+    private lateinit var userNameInput : EditText
+    private lateinit var passWordInput : EditText
+    private lateinit var submitButton : Button
+
+
+
+
+
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
